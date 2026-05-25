6043bf56b78653d082cf5c4819f0c42c5400da6f
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2026-05-25 21:39:34 +0530

    Add files via upload

---
+++ QuestionCard.tsx
@@ -0,0 +1,76 @@
+// export default function QuestionCard({ question }:any){
+//     return(
+//         <div className="border rounded-xl p-4 mb-4">
+//             <h3 className="font-semibold">
+//                 {question.question}
+//             </h3>
+//             <div className="flex gap-3 mt-3">
+//                 <span>{question.difficulty}</span>
+//                 <span>{question.marks} Marks</span>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+export default function QuestionCard({
+  question
+}: any) {
+
+  return (
+
+    <div
+      className="
+      border
+      rounded-2xl
+      p-5
+      mb-4
+      bg-gray-50
+      "
+    >
+
+      <h3
+        className="
+        font-semibold
+        text-lg
+        "
+      >
+        {question.question}
+      </h3>
+
+      <div
+        className="
+        flex
+        gap-3
+        mt-4
+        items-center
+        "
+      >
+
+        <span
+          className="
+          px-3
+          py-1
+          rounded-full
+          text-sm
+          bg-black
+          text-white
+          "
+        >
+          {question.difficulty}
+        </span>
+
+        <span
+          className="
+          text-sm
+          font-medium
+          "
+        >
+          {question.marks} Marks
+        </span>
+
+      </div>
+
+    </div>
+  )
+}

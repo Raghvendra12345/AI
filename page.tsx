5e123cd34da5b115a9bb128d80c013114e1142c7
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2026-05-25 21:23:59 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,78 @@
+// import DashboardLayout
+// from '@/components/layout/DashboardLayout'
+
+// export default function HomePage() {
+
+//   return (
+
+//     <DashboardLayout>
+
+//       <div
+//         className="
+//         bg-white
+//         rounded-3xl
+//         p-10
+//         shadow-sm
+//         "
+//       >
+
+//         <h1
+//           className="
+//           text-4xl
+//           font-bold
+//           mb-4
+//           "
+//         >
+//           AI Assessment Creator
+//         </h1>
+
+//         <p className="text-gray-500">
+//           Create AI-powered
+//           assignments instantly.
+//         </p>
+
+//       </div>
+
+//     </DashboardLayout>
+//   )
+// }
+
+
+import DashboardLayout
+from '@/components/layout/DashboardLayout'
+
+export default function HomePage() {
+
+  return (
+
+    <DashboardLayout>
+
+      <div
+        className="
+        bg-white
+        rounded-3xl
+        p-10
+        shadow-sm
+        "
+      >
+
+        <h1
+          className="
+          text-4xl
+          font-bold
+          mb-4
+          "
+        >
+          AI Assessment Creator
+        </h1>
+
+        <p className="text-gray-500">
+          Create AI-powered
+          assignments instantly.
+        </p>
+
+      </div>
+
+    </DashboardLayout>
+  )
+}

89e729eac558ee6e7cef46202a31e3d8dbae685c
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2026-05-26 10:15:12 +0530

    Delete assignment.queue.ts

--- assignment.queue.ts
+++
@@ -1,8 +0,0 @@
-import {Queue} from 'bullmq'
-import {redisConnection} from '../config/redis'
-
-
-
-export const assessmentQueue=new Queue('assessment-generation',{
-    connection:redisConnection,
-})

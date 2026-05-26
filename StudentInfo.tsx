e7f358f4501a0151162ba5c8796575b2ad14cb07
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2026-05-26 10:15:34 +0530

    Delete StudentInfo.tsx

--- StudentInfo.tsx
+++
@@ -1,9 +0,0 @@
-export default function StudentInfo(){
-    return(
-        <div className="border-b pb-6 mb-6">
-            <div>Name:___________________</div>
-            <div>Roll No:________________</div>
-            <div>Section:________________</div>
-        </div>
-    )
-}
